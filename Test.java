013f39c99f26d803042ab7100dbfe513adc407cc
Marelly <ramimarelly@gmail.com>
2018-07-09 19:53:10 +0300

    Commit after new git

--- Test.java
+++ Test.java
@@ -2,6 +2,7 @@ public class Test{
     public static void main(String[] args) {
         String x = "Test";
         String y = "Test5";
-        System.out.println(x);
+        String z = "Test6";
+        System.out.println(x+y+z);
     }
 }
