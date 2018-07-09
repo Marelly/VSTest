4409da56875c66c04e5f55a0cd45e71df322428f
Marelly <ramimarelly@gmail.com>
2018-07-09 20:05:09 +0300

    Test7

--- Test.java
+++ Test.java
@@ -2,7 +2,7 @@ public class Test{
     public static void main(String[] args) {
         String x = "Test3";
         String y = "Test5";
-        String z = "Test6";
+        String z = "Test7";
         System.out.println(x+y+z);
     }
 }
