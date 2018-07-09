e73b634e6c1abeded99c4fe309e0265aa896bde6
Marelly <ramimarelly@gmail.com>
2018-07-09 19:57:22 +0300

    Second commit

--- Test.java
+++ Test.java
@@ -1,6 +1,6 @@
 public class Test{
     public static void main(String[] args) {
-        String x = "Test";
+        String x = "Test3";
         String y = "Test5";
         String z = "Test6";
         System.out.println(x+y+z);
